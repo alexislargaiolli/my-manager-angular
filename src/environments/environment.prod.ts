d077eca7b22beb48b196ced5bccf6af985edb518
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-05-15 18:12:27 +0200

    Fix compilation problems

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  api_endpoint: 'https://my-manager-api.herokuapp.com/api/v1'
+  api_endpoint: 'https://my-manager-api.herokuapp.com/api/v1',
+  login_endpoint: 'mmusers/login',
+  logout_endpoint: 'mmusers/logout'
 };
