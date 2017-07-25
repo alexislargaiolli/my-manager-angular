348814a1b34a0ab3d09b552c3993dc8d677f166e
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-07-25 17:55:26 +0200

    Responsive improvements

--- src/app/modules/project/components/common/create-note/create-note.component.ts
+++ src/app/modules/project/components/common/create-note/create-note.component.ts
@@ -6,7 +6,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-create-note',
   templateUrl: './create-note.component.html',
-  styleUrls: ['./create-note.component.css'],
+  styleUrls: ['./create-note.component.scss'],
 })
 export class CreateNoteComponent implements OnInit {
 
