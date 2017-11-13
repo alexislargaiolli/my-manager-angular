fcbe3eb084eff1e070657a03c0705c9da7f3b24a
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-11-13 10:32:56 +0100

    Fix assets path in server.js

--- dist/server.js
+++ dist/server.js
@@ -23,11 +23,11 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + ''));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 // Start the app by listening on the default
 // Heroku port
